c11cd43be7343b28ebaa56a7e4892d3ab9a547f8
Marti <118295021+itdeveloppement@users.noreply.github.com>
2024-07-03 14:42:41 +0200

    init connexion serveur

---
+++ Config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// bdd
+// DB_DATABASE=jmwd5247_portfolio 
+// DB_USERNAME=jmwd5247_marti 
+// DB_PASSWORD=jmwd47
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h2>En travaux. Bientot accessible !</h2>
+<img src="public/images/travaux.img" alt="">
+</body>
+</html>
